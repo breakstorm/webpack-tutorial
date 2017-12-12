65c004cc994fe7bfc3d06d405a2d2ebaf3b0c48a
김휘겸 <breakstorm00@gmail.com>
2017-12-12 15:13:53 +0900

    Update npm module import (모듈 불러와서 사용하는 방법)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import printMe from './print.js';
+import * as d3 from 'd3';
 
 function component() {
   var element = document.createElement('div');
@@ -16,3 +17,10 @@ function component() {
 }
 
 document.body.appendChild(component());
+
+d3.select('body').append('svg')
+d3.select("svg").append("line").attr("x1", 20).attr("y1", 20).attr("x2",400).attr("y2",400).style("stroke", "black").style("stroke-width","2px");
+d3.select("svg").append("text").attr("x",20).attr("y",20).text("HELLO");
+d3.select("svg").append("circle").attr("r", 20).attr("cx",20).attr("cy",20).style("fill","red");
+d3.select("svg").append("circle").attr("r", 100).attr("cx",400).attr("cy",400).style("fill","lightblue");
+d3.select("svg").append("text").attr("x",400).attr("y",400).text("WORLD");
